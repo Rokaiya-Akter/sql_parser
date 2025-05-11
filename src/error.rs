888a5bf9a7c8084f635138b7f35464d19d00dae6
Rokaiya-Akter <rokaiyaakter185@gmail.com>
2025-05-11 23:06:55 +0300

    Update error.rs

--- src/error.rs
+++ src/error.rs
@@ -1,15 +1,5 @@
 #[derive(Debug)]
 pub enum ParseError {
-    ExpectedToken(String),
-    ExpectedIdentifier,
-    ExpectedType,
-    UnexpectedToken(String),
-    UnexpectedChar(char),
     TokenizerError(String),
-}
-
-impl From<String> for ParseError {
-    fn from(msg: String) -> Self {
-        ParseError::TokenizerError(msg)
-    }
+    Unsupported(String),
 }
